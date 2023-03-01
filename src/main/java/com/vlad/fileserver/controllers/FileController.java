5fee69b1e5921a274675c3c2f6a5abe71066162a
Vlad <azaz08082001@gmail.com>
2023-03-01 21:19:34 +0500

    little change

--- src/main/java/com/vlad/fileserver/controllers/FileController.java
+++ src/main/java/com/vlad/fileserver/controllers/FileController.java
@@ -1,6 +1,6 @@
 package com.vlad.fileserver.controllers;
 
-import com.vlad.fileserver.models.File;
+
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -10,36 +10,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.*;
 
 
 @RestController
 public class FileController {
-    @GetMapping("/file")
-    public File getFile(){
-        File file=new File();
-        file.setName("test");
-        file.setId(1);
-        return file;
-    }
+
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file){
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file){
         if (!file.isEmpty()) {
             try {
+                File f = new File(".//files//"+file.getOriginalFilename());
+                if(f.exists() && f.isFile()) {
+                    return ResponseEntity.badRequest().body("Файл уже существует");
+                }
                 byte[] bytes = file.getBytes();
                 BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new java.io.File(".//files//"+file.getOriginalFilename())));
+                        new BufferedOutputStream(new FileOutputStream(".//files//"+file.getOriginalFilename()));
                 stream.write(bytes);
                 stream.close();
-                return "ok";
+                return ResponseEntity.ok("Файл загружен");
             } catch (Exception e) {
-                return "Вам не удалось загрузить " +" => " + e.getMessage();
+                return ResponseEntity.badRequest().body("Вам не удалось загрузить " +" => " + e.getMessage());
             }
         } else {
-            return "Вам не удалось загрузить "  + " потому что файл пустой.";
+            return ResponseEntity.unprocessableEntity().body("Вам не удалось загрузить "  + " потому что файл пустой.");
         }
     }
     @GetMapping("/download/{filename}")
@@ -60,7 +55,7 @@ public class FileController {
                 .headers(header)
                 .contentLength(downloadFile.length())
 //                .contentType(MediaType.parseMediaType("application/octet-stream"))
-                .contentType(MediaType.IMAGE_JPEG)
+//                .contentType(MediaType.IMAGE_JPEG)
                 .body(resource);
 
     }
--- src/main/java/com/vlad/fileserver/models/File.java
+++
@@ -1,9 +0,0 @@
-package com.vlad.fileserver.models;
-
-import lombok.Data;
-
-@Data
-public class File {
-    private int id;
-    private String name;
-}
